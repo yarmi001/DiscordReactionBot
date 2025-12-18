cbf32f69a38ff2e3e7bbc116b042d0a471d044d0
yarmi001 <158520404+djdizel@users.noreply.github.com>
2025-12-18 18:17:04 +0200

    2 stage

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System.Text.Json;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
 // Чтение конфигурационного файла
 if (!File.Exists("config.json"))
 {
@@ -9,10 +12,18 @@ if (!File.Exists("config.json"))
 }
 // Десериализация конфигурации
 string jsonString = await File.ReadAllTextAsync("config.json");
+
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+    ReadCommentHandling = JsonCommentHandling.Skip
+};
+
 var config = JsonSerializer.Deserialize<BotConfig>(jsonString);
 // Проверка наличия токена
-if (string.IsNullOrEmpty(config?.Token))
+if (string.IsNullOrEmpty(config?.Token) || config.TargetUserId == 0)
 {
+    Console.WriteLine("ID = 0 или пустой токен в config.json");
     Console.WriteLine("Токен не найден внутри config.json");
     return;
 }
@@ -24,6 +35,7 @@ var socketConfig = new DiscordSocketConfig
 };
 
 using var client = new DiscordSocketClient(socketConfig);// Создание клиента
+
 // Обработчики событий
 client.Log += (LogMessage log) => 
 {
@@ -33,9 +45,35 @@ client.Log += (LogMessage log) =>
 // Событие готовности
 client.Ready += () => 
 {
-    Console.WriteLine("\nБот подключен");
+    Console.WriteLine($"\nБот подключен. Цель(ID): {config.TargetUserId}, Реакция: {config.ReactionEmoji}\n");
     return Task.CompletedTask;
 };
+
+client.MessageReceived += async (SocketMessage message) => 
+{
+    // Игнорирование сообщений от ботов
+    if (message.Author.IsBot) return;
+
+    // Проверка на целевого пользователя
+    if (message.Author.Id == config.TargetUserId)
+    {
+        try
+        {
+            Console.WriteLine($"[Detect] Сообщение от {message.Author.Username}. Ставлю реакцию...");
+            
+            // Создаем эмодзи из строки в конфиге
+            var emoji = new Emoji(config.ReactionEmoji);
+            
+            // Ставим реакцию
+            await message.AddReactionAsync(emoji);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Error] Не удалось поставить реакцию: {ex.Message}");
+        }
+    }
+};
+
 // Вход и запуск бота
 try 
 {
@@ -54,4 +92,6 @@ await Task.Delay(-1);
 class BotConfig // Класс конфигурации бота
 {
     public string Token { get; set; } = "";
+    public ulong TargetUserId { get; set; }
+    public string ReactionEmoji { get; set; } = "🐳";
 }
